43ad13f77ba56c1169552e9b4c68a35291ceb417
Samuel <samuelsantanadasilva8@gmail.com>
2022-11-04 20:34:49 -0300

    atualizar

---
+++ certidoes/certidaoJs/script.js
@@ -0,0 +1,9 @@
+function menu(){
+    const menu = document.querySelector('#menu')
+
+    if(menu.style.display == 'block'){
+        menu.style.display ='none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
